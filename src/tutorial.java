8765b9e2b58b75e80d590a0df9d12f3726568106
quodnon <feanorfeanturi@gmail.com>
2014-12-08 21:39:19 +0200

    initiaal

---
+++ src/tutorial.java
@@ -0,0 +1,12 @@
+/**
+ * Created by quodnon on 08.12.14.
+ */
+import  java.*;
+import  java.io.*;
+public class tutorial {
+    public  static void main(String [] args){
+        System.out.println("Hello wordl");
+        MakeCanine s=new MakeCanine();
+        s.go();
+    }
+}
