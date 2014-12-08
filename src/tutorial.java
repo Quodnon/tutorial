9f4f45b30c0a2c3a890cdddecbb7fa1a8d351a80
quodnon <feanorfeanturi@gmail.com>
2014-12-08 23:06:05 +0200

    2

--- src/tutorial.java
+++ src/tutorial.java
@@ -1,19 +1,23 @@
 /**
  * Created by quodnon on 08.12.14.
  */
+import org.omg.PortableServer._ServantLocatorStub;
+
 import  java.*;
 import  java.io.*;
 public class tutorial {
     public  static void main(String [] args){
-        System.out.println("Hello wordl");
-        MakeCanine s=new MakeCanine();
-        s.go();
-        MyAnimalList alist = new MyAnimalList();
-        Dog d = new Dog();
-        Cat df = new Cat();
-        alist.add(d);
-        alist.add(df);
-        alist.add(d);
-        alist.add(df);;
+        Dog a = new Dog();
+        Cat f = new Cat();
+        Cat sf = new Cat();
+
+        if(a.equals(f) ){
+            System.out.print("true");
+
+        } else {
+            System.out.print("false");
+
+        }
+        System.out.print( sf.hashCode()+" "+ sf.getClass() +' '+ sf.toString());
     }
 }
